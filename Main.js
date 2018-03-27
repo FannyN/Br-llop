580d952ff23f08d5cb0f45290fe3d9a9f2d6c22f
Fanny <fanny.nylund@hotmail.com>
2018-03-27 10:29:37 +0200

    Initial Commit

---
+++ Main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.navigation-icon').click(function() {
+        $('nav ul').toggleClass('open')
+    })
+})
+
+
+// var sayHello = function(helloMessage) { 
+//     console.log(helloMessage)
+// }
